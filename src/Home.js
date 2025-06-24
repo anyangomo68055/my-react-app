dd89645d66dead1016b56df7dadc2fe9c72ea2d6
anyangomo68055 <anyangomolouisanze@gmail.com>
2025-06-24 09:00:22 +0100

    Refactor application: remove unused files, implement Navbar and Home components, and update styles for new layout

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import MovieList from './MovieList';
+
+
+const Home = () => {
+  const [movies, setMovies] = useState(null)
+
+useEffect(()=>{
+  fetch('http://localhost:8000/movies')
+  .then((res)=>{
+    return res.json()
+  })
+  .then((data)=>{
+    console.log(data)
+    setMovies(data)
+  })
+}, [])
+
+
+  
+ 
+  return (
+    <div className='home'>
+      <MovieList movies = {movies} title = "Movie's Blo;!!!" />
+
+
+    </div>
+  )     
+}
+
+export default Home
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>my_Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+
